cd791fb741c58f2c4d6442bfa116efb8e787fac5
BVantur <blaz.vantur@gmail.com>
2021-01-24 17:21:33 +0100

    refactor: custom layout for Snackbar

--- app/src/main/java/sp/bvantur/deprecatedtoast/utils/SnackbarUtils.kt
+++ app/src/main/java/sp/bvantur/deprecatedtoast/utils/SnackbarUtils.kt
@@ -3,10 +3,14 @@ package sp.bvantur.deprecatedtoast.utils
 import android.app.Activity
 import android.graphics.Color
 import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
-import com.google.android.material.snackbar.BaseTransientBottomBar.ANIMATION_MODE_FADE
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import sp.bvantur.deprecatedtoast.R
 
@@ -32,20 +36,88 @@ object SnackbarUtils {
         textMessage: String,
         backgroundColor: Int = ContextCompat.getColor(activity, R.color.teal_200)
     ) {
-        val snackbar = Snackbar.make(activity.findViewById(android.R.id.content), textMessage, Snackbar.LENGTH_LONG)
-        snackbar.view.setBackgroundColor(backgroundColor)
-        setCorrectAnimationAndPosition(snackbar)
-        snackbar.show()
+        SimpleCustomSnackbar.make(activity, textMessage, backgroundColor)?.show()
+    }
+}
+
+class SimpleCustomSnackbar(parent: ViewGroup, content: View) :
+    BaseTransientBottomBar<SimpleCustomSnackbar?>(
+        parent,
+        content,
+        object : com.google.android.material.snackbar.ContentViewCallback {
+            override fun animateContentIn(delay: Int, duration: Int) {}
+            override fun animateContentOut(delay: Int, duration: Int) {}
+        }) {
+
+    init {
+        getView().apply {
+            setBackgroundColor(ContextCompat.getColor(view.context, android.R.color.transparent))
+            setPadding(0, 0, 0, 0)
+        }
     }
 
-    private fun setCorrectAnimationAndPosition(snackbar: Snackbar) {
-        val params = snackbar.view.layoutParams
-        if (params is CoordinatorLayout.LayoutParams) {
-            params.gravity = Gravity.TOP
-        } else if (params is FrameLayout.LayoutParams) {
-            params.gravity = Gravity.TOP
+    companion object {
+        private fun findSuitableParent(view: View?): ViewGroup? {
+            if (view == null) {
+                return null
+            }
+
+            var viewInProcess = view
+            val viewGroup = viewInProcess
+            var fallback: ViewGroup? = null
+            do {
+                if (viewInProcess is CoordinatorLayout) {
+                    return viewGroup as ViewGroup
+                } else if (viewInProcess is FrameLayout) {
+                    if (viewGroup.id == R.id.content) {
+                        return viewInProcess
+                    }
+                    fallback = viewGroup as ViewGroup?
+                }
+
+                if (viewInProcess != null) {
+                    val parent = viewInProcess.parent
+                    viewInProcess = if (parent is View) parent as ViewGroup else null
+                }
+            } while (viewInProcess != null)
+            return fallback
         }
-        snackbar.view.layoutParams = params
-        snackbar.animationMode = ANIMATION_MODE_FADE
+
+        fun make(
+            activity: Activity,
+            message: String,
+            backgroundColor: Int = ContextCompat.getColor(activity, R.color.teal_200),
+            duration: Int = Snackbar.LENGTH_LONG
+        ): SimpleCustomSnackbar? {
+            val parent =
+                findSuitableParent(activity.findViewById(android.R.id.content)) ?: return null
+
+            try {
+                val toastView: View = LayoutInflater.from(activity)
+                    .inflate(R.layout.layout_custom_snackbar, parent, false)
+                (toastView.findViewById<View>(R.id.tvText) as TextView).text = message
+                toastView.setBackgroundColor(backgroundColor)
+                val snackbar = SimpleCustomSnackbar(
+                    parent,
+                    toastView
+                ).setDuration(duration)
+                setCorrectAnimationAndPosition(snackbar)
+                return snackbar
+            } catch (e: Exception) {
+                println(e.message)
+            }
+            return null
+        }
+
+        private fun setCorrectAnimationAndPosition(snackbar: SimpleCustomSnackbar) {
+            val params = snackbar.view.layoutParams
+            if (params is CoordinatorLayout.LayoutParams) {
+                params.gravity = Gravity.TOP
+            } else if (params is FrameLayout.LayoutParams) {
+                params.gravity = Gravity.TOP
+            }
+            snackbar.view.layoutParams = params
+            snackbar.animationMode = ANIMATION_MODE_FADE
+        }
     }
 }
